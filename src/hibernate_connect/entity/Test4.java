1c9140918d517f0822330d9559d5576585924f18
Pavel <pavel.fortex@yandex.ru>
2022-12-26 13:23:12 +0300

    Получаем оббъекты из mySQL в Java

---
+++ src/hibernate_connect/entity/Test4.java
@@ -0,0 +1,32 @@
+package hibernate_connect.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test4 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()        //создаем сесию
+                .configure("hibernate.cfg.xml")    //говорим, как создавать сессию
+                .addAnnotatedClass(Employee.class)          //класс, в котором есть аннотации для работы с базой
+                .buildSessionFactory();                     //строит саму сессию
+
+        try {
+            Session session = factory.getCurrentSession();
+
+            session.beginTransaction();  //начинаем транзакцию
+
+            /*Employee emp = session.get(Employee.class,1);             // id работника 1
+            emp.setSalary(1000);      */                                // поменяли зарплату
+
+            session.createQuery("update Employee set salary = 1000 where first_name = 'Maksim'");
+            session.getTransaction().commit();  // конец транзакции
+
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
